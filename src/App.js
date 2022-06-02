97a30ccbf49c976e18b908dcd8f373e74e226dcf
Anupam Dey <79352396+anupamdey2001@users.noreply.github.com>
2022-06-02 08:23:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
